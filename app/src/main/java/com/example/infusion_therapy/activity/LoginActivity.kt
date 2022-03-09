a892ff5d8c903f2702ba6b70290da332daf2a8cc
Shree <bornarekalpesh@gmail.com>
2022-03-09 13:28:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/infusion_therapy/activity/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.infusion_therapy.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.infusion_therapy.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
